e02ced63c9d87994f0ea5730906a253470286289
zhou <2601846208@qq.com>
2026-09-11 18:41:51 +0800

    ✨ feat: 生产检验模式支持查看测量记忆
    - 生产检验模式长按记忆键后可进入历史记录浏览：移除 Insptectmode 对记忆模式入口的限制

--- Src/Drv_Key.c
+++ Src/Drv_Key.c
@@ -83,7 +83,7 @@ void App_MemKeyProcess(void)
     if( uKeyPress.bits.MemKeyPress )									//开机键按下
     {
         uKeyRelease.bits.MemKeyRelease = 0;
-        if( uKeyHold.bits.MemKeyHold && !F_MemKey_Deal && !uErrFlag.bits.Er2 && !uErrFlag.bits.Er6 && eTestmode_num != Insptectmode )			//生产模式无记忆
+        if( uKeyHold.bits.MemKeyHold && !F_MemKey_Deal && !uErrFlag.bits.Er2 && !uErrFlag.bits.Er6 )
         {
             F_MemKey_Deal = 1;
             uKeyHold.bits.MemKeyHold = 0;
